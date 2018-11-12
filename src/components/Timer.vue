81e1f156f39e0b78eba9d7313f5e4697a561dea8
mfertin <pdypl-4@sr.dmcs.pl>
2018-11-12 16:27:02 +0100

    fix Timer.vue merge

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,19 +1,10 @@
 <template>
-<<<<<<< HEAD
     <v-card height="100%" class="widget">
-=======
-    <div>
->>>>>>> 1fdfc253e84f2657fb5b3d63ba53f06cae28bfd5
         <div class="card-title"><h1>Timer</h1></div>
         <div class="timer">
             {{days}} jours {{hours}}h {{minutes}}m {{seconds}}s
         </div>
-<<<<<<< HEAD
     </v-card>
-
-=======
-    </div>
->>>>>>> 1fdfc253e84f2657fb5b3d63ba53f06cae28bfd5
 </template>
 
 <script>
@@ -62,22 +53,6 @@
 </script>
 
 <style scoped>
-
-<<<<<<< HEAD
-=======
-    /*.card{
-        box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-        transition: 0.3s;
-        border-radius: 5px; /* 5px rounded corners *//*
-        max-width: 300px;
-        margin: 2%;
-    }
-
-    .card:hover {
-        box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
-    }*/
-
->>>>>>> 1fdfc253e84f2657fb5b3d63ba53f06cae28bfd5
     .card-title {
         padding: 2px 16px;
         margin: 2%;
