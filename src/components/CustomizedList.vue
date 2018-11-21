bda8544377b4438ddb50531de3c5c1cc681ed877
mfertin <pdypl-4@sr.dmcs.pl>
2018-11-21 10:52:48 +0100

    fix error in ChartArea and add API call to CustomizedList

--- src/components/CustomizedList.vue
+++ src/components/CustomizedList.vue
@@ -2,78 +2,53 @@
     <v-card height="94%" class="widget">
     <!-- Material Design Icons are not working for now-->
         <v-data-table height="100%"
-                :hide-headers="true"
                 :hide-actions="true"
-                :items="desserts"
+                :headers="headers"
+                :items="batteryLevels"
                 class="elevation-1"
-                :dark="true"
         >
             <template slot="items" slot-scope="props">
-                <td>{{ props.item.name }}</td>
-                <td class="text-xs-right">{{ props.item.calories }}</td>
+                <td>{{ props.item.mac }}</td>
+                <td class="text-xs-right">{{ props.item.batteryLevel }}</td>
             </template>
         </v-data-table>
     </v-card>
 </template>
 
 <script>
+
+    import NodesAPI from '../services/api/Nodes'
+
     export default {
         name: "CustomizedList",
         data () {
             return {
-                desserts: [
+                headers: [
                     {
-                        value: true,
-                        name: 'Frozen Yogurt',
-                        calories: 159
+                        text: 'MAC Number',
+                        align: 'left',
+                        sortable: false,
+                        value: 'mac'
                     },
-                    {
-                        value: true,
-                        name: 'Ice cream sandwich',
-                        calories: 237
-                    },
-                    {
-                        value: true,
-                        name: 'Eclair',
-                        calories: 262
-                    },
-                    {
-                        value: true,
-                        name: 'Cupcake',
-                        calories: 305
-                    },
-                    {
-                        value: true,
-                        name: 'Gingerbread',
-                        calories: 356
-                    },
-                    {
-                        value: true,
-                        name: 'Jelly bean',
-                        calories: 375
-                    },
-                    {
-                        value: true,
-                        name: 'Lollipop',
-                        calories: 392
-                    },
-                    {
-                        value: true,
-                        name: 'Honeycomb',
-                        calories: 408
-                    },
-                    {
-                        value: true,
-                        name: 'Donut',
-                        calories: 452
-                    },
-                    {
-                        value: true,
-                        name: 'KitKat',
-                        calories: 518
-                    }
-                ]
+                    { text: 'Battery Level (%)', value: 'batteryLevel' }
+                ],
+                batteryLevels: []
             }
+        },
+        mounted: function() {
+            NodesAPI.getNodes()
+                .then(nodes => {
+                    let lowBatteries = []
+                    for (let i in nodes){
+                        if (nodes[i].batteryLevel <=30) {
+                            lowBatteries.push({
+                                mac: nodes[i].mac,
+                                batteryLevel: nodes[i].batteryLevel
+                            })
+                        }
+                    }
+                    this.batteryLevels = lowBatteries
+                })
         }
     }
 </script>
