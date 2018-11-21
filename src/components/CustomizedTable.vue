f2927186bca1deb5382852cde2d3dd55456da0b6
Malefor0 <david.bricenoa@gmail.com>
2018-11-21 16:28:52 +0100

    add dialog for battery history on data table

--- src/components/CustomizedTable.vue
+++ src/components/CustomizedTable.vue
@@ -7,31 +7,18 @@
                 class="elevation-1"
         >
             <template slot="items" slot-scope="props">
-                <td>
-                    <template>
-                        <div class="text-xs-center">
-                            <v-dialog v-model="dialog" width="500">
-                                <v-btn slot="activator" color="red lighten-2" dark>
-                                    {{ props.item.mac }}
-                                </v-btn>
-                                <v-card height="94%" class="widget" overflow-hidden>
-                                    <v-card-title class="headline grey lighten-2" primary-title>
-                                        Pourcentage de Batterie
-                                    </v-card-title>
-                                    <v-card-text>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                                    </v-card-text>
-                                </v-card>
-                                <v-card-actions>
-                                    <v-spacer></v-spacer>
-                                    <v-btn color="primary" flat @click="dialog = false">
-                                        Close
-                                    </v-btn>
-                                </v-card-actions>
-                            </v-dialog>
-                        </div>
-                    </template>
-                </td>
+                <td class="text-xs-right">
+                    <v-dialog v-model="dialog" lazy absolute>
+                        <v-btn primary dark slot="activator">{{ props.item.mac }}</v-btn>
+                        <v-card>
+                            <v-card-title>
+                                <div class="headline">Batterie de {{ props.item.mac }}</div>
+                            </v-card-title>
+                            <v-card-text>
+                                <BatteryHistoryChart :mac="props.item.mac" :batteryHistory="JSON.parse(JSON.stringify(props.item.batteryHistory))"></BatteryHistoryChart>
+                            </v-card-text>
+                        </v-card>
+                    </v-dialog></td>
                 <td class="text-xs-right">{{ props.item.serialnumber }}</td>
                 <td class="text-xs-right">{{ props.item.firmware }}</td>
                 <td class="text-xs-right">{{ props.item.batterylvl }}</td>
@@ -46,9 +33,15 @@
 
 <script>
     import NodesAPI from '../services/api/Nodes'
+    import Top3 from './Top3'
+    import ChartCandle from './ChartCandle'
+    import BatteryHistoryChart from './BatteryHistoryChart'
 
     export default {
         name: "CustomizedTable",
+        components:{
+          Top3, ChartCandle, BatteryHistoryChart
+        },
         data () {
             return {
                 headers: [
@@ -67,20 +60,24 @@
                 dialog: false
             }
         },
+
         mounted() {
             NodesAPI.getNodes()
                 .then(node => {
                     for (let o of node){
-                        var line = {};
+                        let line = {};
                         line.mac = o.mac;
                         line.serialnumber = o.serialNumber;
                         line.firmware = o.nodeFirmware.description;
                         line.batterylvl = o.batteryLevel;
                         line.info = o.lastInfo;
+                        line.batteryHistory = o.batteryHistory
                         this.nodes.push(line);
                     }
-
                 })
+
+
+
         }
     }
 </script>
